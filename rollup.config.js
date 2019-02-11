115e1ed1e53e766ab8c97d359cbc15bd8cc57859
Hearty Oh <heartyoh@hatiolab.com>
2019-02-11 19:03:51 +0900

    rollup

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+
+let pkg = require("./package.json");
+let external = Object.keys(pkg.dependencies);
+let plugins = [
+  resolve(),
+  babel(),
+  commonjs(),
+  terser({
+    sourcemap: true
+  })
+];
+
+export default [
+  {
+    input: "src/index.js",
+    plugins,
+    external,
+    output: [
+      {
+        file: pkg.main,
+        name: "things-scene-restful",
+        format: "umd",
+        globals: {
+          "@hatiolab/things-scene": "scene"
+        }
+      }
+    ]
+  },
+  {
+    input: "src/index.js",
+    plugins,
+    external,
+    output: [
+      {
+        file: pkg.module,
+        format: "esm"
+      }
+    ]
+  }
+];
